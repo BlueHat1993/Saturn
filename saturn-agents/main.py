363304e5794fc1f29aafc704af7ba122c5c8f8d0
Nilava <nilavabiswas93@gmail.com>
2026-05-25 00:46:25 +0530

    test deepseek commit

--- saturn-agents/main.py
+++ saturn-agents/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from saturn-agents!")
+from langchain.agents import create_agent 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+agent = create_agent(
+        model="deepseek:deepseek-chat",
+        tools=[],
+        system_prompt="You are a helpful assistant that provides information about the world."
+)
 
 
 if __name__ == "__main__":
-    main()
+    result = agent.invoke(
+        {"messages": [{"role": "user", "content": "Hi,how are you?"}]}
+    )
+    print(result["messages"][-1].text)
